927335cfac08689a9bcb66e0004ce8da11bcf8fc
uuga8 <76564742+uuga8@users.noreply.github.com>
2021-02-01 22:00:13 +0200

    Update 1137. N-th Tribonacci Number.py

--- python/1137. N-th Tribonacci Number.py
+++ python/1137. N-th Tribonacci Number.py
@@ -9,3 +9,25 @@ class Solution(object):
             memory[key] = memory[key-1] + memory[key-2] + memory[key-3]
         
         return memory[n]
+
+    
+class Solution(object):
+    def tribonacci(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        if n < 1:
+            return 0
+        if n < 3:
+            return 1
+        else:
+            zero = 0
+            one = 1
+            two = 1
+            for key in range(3, n + 1):
+                next = zero + one + two
+                zero = one
+                one = two
+                two = next
+            return next
