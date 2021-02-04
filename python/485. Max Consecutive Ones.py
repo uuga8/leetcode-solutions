f921916ceedd884a239015988b8663128bcfc52e
uuga8 <76564742+uuga8@users.noreply.github.com>
2021-02-04 20:50:37 +0200

    Update 485. Max Consecutive Ones.py

--- python/485. Max Consecutive Ones.py
+++ python/485. Max Consecutive Ones.py
@@ -15,3 +15,24 @@ class Solution(object):
                 count = 0
                 
         return 0 if not consecutives else max(consecutives)
+
+    
+    
+    
+class Solution(object):
+    def findMaxConsecutiveOnes(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        current_count = 0
+        highest_count = 0
+        
+        for index in range(0, len(nums)):
+            if nums[index] == 1:
+                current_count += 1
+            if (nums[index] != 1 or index == len(nums) - 1) and current_count > 0:
+                if current_count > highest_count:
+                    highest_count = current_count
+                current_count = 0
+        return highest_count
