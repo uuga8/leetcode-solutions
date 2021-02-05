418dd729a99737dff5c9182c55ededbef7af15e3
uuga8 <76564742+uuga8@users.noreply.github.com>
2021-02-05 14:38:39 +0200

    Update 1295. Find Numbers with Even Number of Digits.py

--- python/1295. Find Numbers with Even Number of Digits.py
+++ python/1295. Find Numbers with Even Number of Digits.py
@@ -13,3 +13,21 @@ class Solution(object):
                 even += 1
         return even
         
+class Solution(object):
+    def findNumbers(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        even = 0
+        count = 0
+        
+        for index in range(0, len(nums)):
+            remainder = nums[index]
+            while remainder >= 1:
+                remainder = remainder / 10
+                count += 1
+            if count % 2 == 0:
+                even += 1
+            count = 0
+        return even
