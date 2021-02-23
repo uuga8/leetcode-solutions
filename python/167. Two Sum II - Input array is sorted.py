6187392b45ff66098160a8843520689c865e6554
uuga8 <76564742+uuga8@users.noreply.github.com>
2021-02-23 19:14:34 +0200

    Update 167. Two Sum II - Input array is sorted.py

--- python/167. Two Sum II - Input array is sorted.py
+++ python/167. Two Sum II - Input array is sorted.py
@@ -13,3 +13,17 @@ class Solution:
             else:
                 return [seen[first_half] + 1, index + 1]
             
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+           
+        right = len(numbers) - 1
+        left = 0
+        
+        while left < right:
+            first_half = target - numbers[right]
+            if first_half > numbers[left]:
+                left += 1
+            elif first_half < numbers[left]:
+                right -= 1
+            else:
+                return [left + 1, right + 1]
